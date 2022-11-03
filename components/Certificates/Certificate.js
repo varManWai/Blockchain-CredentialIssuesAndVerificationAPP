ac12f7dbd96e33607704ffdfaa41007e66f3dc02
varManWai <laizoke98@gmail.com>
2022-11-03 23:57:38 +0800

    added some components

---
+++ components/Certificates/Certificate.js
@@ -0,0 +1,10 @@
+
+
+export default function CertificateItem() {
+    return (
+        <div>
+            <h1>this is the certificate small component 'show in the all certificates'</h1>
+
+        </div>
+    )
+}
