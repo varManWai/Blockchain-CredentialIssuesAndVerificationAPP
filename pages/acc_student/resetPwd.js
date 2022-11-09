7f410f74d3cc437940b63eac9d6d4478f41e84f8
varManWai <laizoke98@gmail.com>
2022-11-09 23:43:24 +0800

    completed reset password page frontend

--- pages/acc_student/resetPwd.js
+++ pages/acc_student/resetPwd.js
@@ -3,7 +3,6 @@ import Stud_ResetPwd_Form from "../../components/Forms/stud_resetPwd.form";
 
 export default function Login() {
 
-
     return (
         <Stud_Layout imgSrc="/images/forgotPwd.jpg">
             <Stud_ResetPwd_Form />
