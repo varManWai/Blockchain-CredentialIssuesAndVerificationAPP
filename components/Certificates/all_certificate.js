fa33091dc801bd552d0dd763396b6ffef041d740
varManWai <laizoke98@gmail.com>
2022-11-13 23:50:01 +0800

    added the pagination on the all certificates pages

--- components/Certificates/all_certificate.js
+++ components/Certificates/all_certificate.js
@@ -1,10 +1,23 @@
 import CertificateGrid from "./certificate_grid"
 
+import { Pagination } from "antd";
+
 export default function AllCertificate() {
+    const itemRender = (_, type, originalElement) => {
+        if (type === 'prev') {
+          return <a>Previous</a>;
+        }
+        if (type === 'next') {
+          return <a>Next</a>;
+        }
+        return originalElement;
+      };
+
     return (
         <div>
             <h2>this is all certificate</h2>
             <CertificateGrid />
+            <Pagination total={80} itemRender={itemRender} />
         </div>
     )
 }
