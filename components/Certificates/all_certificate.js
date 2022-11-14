42752c9c8c35e9ab70770f18cedcc48d63363b0d
varManWai <laizoke98@gmail.com>
2022-11-14 23:45:48 +0800

    added the add certificate button on all cert

--- components/Certificates/all_certificate.js
+++ components/Certificates/all_certificate.js
@@ -1,6 +1,7 @@
 import CertificateGrid from "./certificate_grid"
 
-import { Pagination } from "antd";
+import { Pagination, Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 import styles from '../../styles/Login.module.css';
 
@@ -8,6 +9,7 @@ export default function AllCertificate() {
 
   return (
     <div className={styles.all_certificates_section}>
+      <Button icon={<PlusOutlined />} type="primary">Primary Button</Button>
       <h2>this is all certificate</h2>
       <CertificateGrid />
     </div>
