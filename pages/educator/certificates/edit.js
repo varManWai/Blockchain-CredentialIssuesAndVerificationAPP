89fa4df5126795a3b2a6236bbd9b7a4bc51fd82a
varManWai <laizoke98@gmail.com>
2022-11-16 13:08:23 +0800

    added some content and direction for the edit certificate page

---
+++ pages/educator/certificates/edit.js
@@ -0,0 +1,9 @@
+import Edit_Certificate from "../../../components/Certificates/edit_certifiicate";
+
+export default function Edit () {
+    return (
+        <div>
+            <Edit_Certificate />
+        </div>
+    )
+}
