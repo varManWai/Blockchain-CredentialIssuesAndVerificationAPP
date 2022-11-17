b2e92a53fbe60c774acf9df9f5fc00963bdacb5d
varManWai <laizoke98@gmail.com>
2022-11-17 23:34:10 +0800

    added the view certificate

--- components/Certificates/view_certificate.js
+++ components/Certificates/view_certificate.js
@@ -3,6 +3,38 @@ export default function View_Certificate () {
     return (
         <div>
             <h1>this is the view certificate page</h1>
+            <Row>
+                <Col>
+                    <Image src="../../public/images/forgotPwd.jpg" />
+                </Col>
+                <Col>
+                    <Row>
+                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>text tex tex</Col>
+                    </Row>
+                    <Row>
+                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>text tex tex</Col>
+                    </Row>
+                    <Row>
+                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>text tex tex</Col>
+                    </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Col><Label>Description</Label></Col>
+                    <Col>description example</Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <Col>
+                    <label htmlFor="">Group</label></Col>
+                    <Col>this is the group text</Col>
+                </Row>
+            </Col>
+            </Row>
         </div>
     )
 }
