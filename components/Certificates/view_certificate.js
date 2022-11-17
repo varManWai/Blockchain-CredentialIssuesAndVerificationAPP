908c56bbccfc5eea1e358269e4d6ede36b726b86
varManWai <laizoke98@gmail.com>
2022-11-17 23:38:41 +0800

    added the edit text to the view certificate page

--- components/Certificates/view_certificate.js
+++ components/Certificates/view_certificate.js
@@ -1,5 +1,5 @@
 
-export default function View_Certificate () {
+export default function View_Certificate() {
     return (
         <div>
             <h1>this is the view certificate page</h1>
@@ -9,31 +9,53 @@ export default function View_Certificate () {
                 </Col>
                 <Col>
                     <Row>
-                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>
+                            <Row>
+                                <Col><label htmlFor="">Id</label></Col>
+                            </Row>
+                        </Col>
                         <Col>text tex tex</Col>
                     </Row>
                     <Row>
-                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>
+                            <Row>
+                                <Col><label htmlFor="">Title</label></Col>
+                                <Col>Edit button</Col>
+                            </Row>
+                        </Col>
                         <Col>text tex tex</Col>
                     </Row>
                     <Row>
-                        <Col><label htmlFor="">Id</label></Col>
+                        <Col>
+                            <Row>
+                                <Col><label htmlFor="">Date and Time</label></Col>
+                            </Row>
+                        </Col>
                         <Col>text tex tex</Col>
                     </Row>
-            </Col>
-            <Col>
-                <Row>
-                    <Col><Label>Description</Label></Col>
-                    <Col>description example</Col>
-                </Row>
-            </Col>
-            <Col>
-                <Row>
-                    <Col>
-                    <label htmlFor="">Group</label></Col>
-                    <Col>this is the group text</Col>
-                </Row>
-            </Col>
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <Col><label htmlFor="">Description</label></Col>
+                                <Col>Edit button</Col>
+                            </Row>
+                        </Col>
+                        <Col>description example</Col>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <Row>
+                                <Col><label htmlFor="">Group</label></Col>
+                                <Col>Edit button</Col>
+                            </Row>
+                        </Col>
+                        <Col>this is the group text</Col>
+                    </Row>
+                </Col>
             </Row>
         </div>
     )
