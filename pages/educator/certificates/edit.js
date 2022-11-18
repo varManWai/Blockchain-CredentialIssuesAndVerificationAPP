13e6793be4ce66b21cc163449f344d22fab2bac8
varManWai <laizoke98@gmail.com>
2022-11-18 23:20:45 +0800

    added the import and render for the edit page testing in edit.js

--- pages/educator/certificates/edit.js
+++ pages/educator/certificates/edit.js
@@ -1,9 +1,13 @@
+import EditTitle from "../../../components/Forms/edu_edit/edu_editTitle_form"
+import EditDescription from "../../../components/Forms/edu_edit/edu_editDesc_form"
+import EditGroup from "../../../components/Forms/edu_edit/edu_editGroup_form"
 
-
-export default function Edit () {
+export default function Edit() {
     return (
         <div>
-            <Edit_Certificate />
+            <EditTitle />
+            {/* <EditDescription />
+            <EditGroup /> */}
         </div>
     )
 }
