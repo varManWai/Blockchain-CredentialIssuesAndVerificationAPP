82e47493c58c2e11dffb9c8c5fc2f76666bc77ca
varManWai <laizoke98@gmail.com>
2022-11-18 23:25:43 +0800

    80 % percent completed the edit title page

--- components/Forms/edu_edit/edu_editTitle_form.js
+++ components/Forms/edu_edit/edu_editTitle_form.js
@@ -1,10 +1,30 @@
+import { Form, Input, Button } from "antd";
+
+import styles from '../../../styles/Login.module.css';
+
 export default function EditTitle() {
   const [form] = Form.useForm();
+
+  const layout = {
+    labelCol: {
+      span: 4,
+    },
+    wrapperCol: {
+      span: 20,
+    },
+  };
+
+  const tailLayout = {
+    wrapperCol: {
+      span: 24,
+    },
+  };
 
   return (
     <div>
       <h1>this is edit title page</h1>
       <Form
+        {...layout}
         name="educator_login"
         className="login-form"
         initialValues={{
@@ -12,12 +32,12 @@ export default function EditTitle() {
         }}
       >
         <Form.Item label="Title">
-          <Input placeholder="your current title"/>
+          <Input placeholder="your current title" />
         </Form.Item>
         <Form.Item label="New Title">
           <Input placeholder="Your new title" />
         </Form.Item>
-        <Form.Item >
+        <Form.Item {...tailLayout} className={styles.addCert_button_container}>
           <Button type="primary">Submit</Button>
         </Form.Item>
       </Form>
