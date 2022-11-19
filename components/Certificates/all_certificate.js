00cc649a7db893e8f78c74ae0e16bb348baf803d
varManWai <laizoke98@gmail.com>
2022-11-19 23:30:08 +0800

    added the input form to the all certificate drawer button

--- components/Certificates/all_certificate.js
+++ components/Certificates/all_certificate.js
@@ -10,6 +10,9 @@ import styles from '../../styles/Login.module.css';
 import { useRouter } from "next/router";
 
 export default function AllCertificate() {
+  const TextArea = Input;
+
+
   const [open, setOpen] = useState(false);
 
   const { Option } = Select;
@@ -55,6 +58,46 @@ export default function AllCertificate() {
 
   const isBreakpoint = useMediaQuery(925)
 
+  const onChange = (value, dateString) => {
+    console.log('Selected Time: ', value);
+    console.log('Formatted Selected Time: ', dateString);
+  };
+
+  const onOk = (value) => {
+    console.log('onOk: ', value);
+  };
+
+
+  const selectContent = [
+    {
+      value: 'jack',
+      label: 'Jack',
+    },
+    {
+      value: 'lucy',
+      label: 'Lucy',
+    },
+    {
+      value: 'disabled',
+      disabled: true,
+      label: 'Disabled',
+    },
+    {
+      value: 'Yiminghe',
+      label: 'yiminghe',
+    },
+  ]
+
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+
+  const tailLayout = {
+    wrapperCol: {
+      span: 24,
+    },
+  };
+
   return (
     <div className={styles.all_certificates_section}>
 
@@ -83,127 +126,36 @@ export default function AllCertificate() {
           <Row gutter={16}>
             <Col span={12}>
               <Form.Item
-                name="name"
-                label="Name"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please enter user name',
-                  },
-                ]}
+                name="title"
+                label="Title"
               >
-                <Input placeholder="Please enter user name" />
+                <Input />
               </Form.Item>
             </Col>
             <Col span={12}>
-              <Form.Item
-                name="url"
-                label="Url"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please enter url',
-                  },
-                ]}
-              >
-                <Input
-                  style={{
-                    width: '100%',
-                  }}
-                  addonBefore="http://"
-                  addonAfter=".com"
-                  placeholder="Please enter url"
-                />
+              <Form.Item label="Date and Time release">
+                <DatePicker showTime onChange={onChange} onOk={onOk} />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={16}>
             <Col span={12}>
-              <Form.Item
-                name="owner"
-                label="Owner"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please select an owner',
-                  },
-                ]}
-              >
-                <Select placeholder="Please select an owner">
-                  <Option value="xiao">Xiaoxiao Fu</Option>
-                  <Option value="mao">Maomao Zhou</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="type"
-                label="Type"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please choose the type',
-                  },
-                ]}
-              >
-                <Select placeholder="Please choose the type">
-                  <Option value="private">Private</Option>
-                  <Option value="public">Public</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Form.Item
-                name="approver"
-                label="Approver"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please choose the approver',
-                  },
-                ]}
-              >
-                <Select placeholder="Please choose the approver">
-                  <Option value="jack">Jack Ma</Option>
-                  <Option value="tom">Tom Liu</Option>
-                </Select>
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                name="dateTime"
-                label="DateTime"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please choose the dateTime',
-                  },
-                ]}
-              >
-                <DatePicker.RangePicker
-                  style={{
-                    width: '100%',
-                  }}
-                  getPopupContainer={(trigger) => trigger.parentElement}
-                />
+              <Form.Item label="Description">
+                <TextArea rows={4} />
               </Form.Item>
             </Col>
           </Row>
           <Row gutter={16}>
             <Col span={24}>
-              <Form.Item
-                name="description"
-                label="Description"
-                rules={[
-                  {
-                    required: true,
-                    message: 'please enter url description',
-                  },
-                ]}
-              >
-                <Input.TextArea rows={4} placeholder="please enter url description" />
+              <Form.Item label="Group">
+                <Select
+                  defaultValue="lucy"
+                  style={{
+                    width: 120,
+                  }}
+                  onChange={handleChange}
+                  options={selectContent}
+                />
               </Form.Item>
             </Col>
           </Row>
