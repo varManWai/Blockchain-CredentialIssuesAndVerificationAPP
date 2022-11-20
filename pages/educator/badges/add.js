340de3d2a0b9cb43a283f1f2b20eba546392ec82
varManWai <laizoke98@gmail.com>
2022-11-20 23:38:10 +0800

    added the header text for the add badge page

--- pages/educator/badges/add.js
+++ pages/educator/badges/add.js
@@ -4,6 +4,7 @@ import AddCertificate from "../../../components/Certificates/add_certificate"
 export default function AddCert() {
     return (
         <div>
+            <h1>this is the add badge page</h1>
             <AddCertificate />
         </div>
     )
