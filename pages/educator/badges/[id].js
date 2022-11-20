39a3375ce4209a2fcbf1cdb02599d3dc4151f092
varManWai <laizoke98@gmail.com>
2022-11-20 23:36:29 +0800

    i added the header text for the view badge pag

--- pages/educator/badges/[id].js
+++ pages/educator/badges/[id].js
@@ -3,6 +3,7 @@ import View_Certificate from "../../../components/Certificates/view_certificate"
 export default function viewCertificate () {
     return (
         <div>
+            <h1>this is the view badges page</h1>
             <View_Certificate />
         </div>
     )
