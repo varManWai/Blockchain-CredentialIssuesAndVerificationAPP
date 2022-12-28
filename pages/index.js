24e2a1f8dfa706d515471a9a700ddf9c8b744b37
varManWai <laizoke98@gmail.com>
2022-12-28 13:52:30 +0800

    done

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+export default function indexedDB() {
+    return (
+        <div>
+            <p>this is home page</p>
+        </div>
+    )
+}
+
+export const getServerSideProps = async (context) => {
+
+    try {
+
+        return {
+            redirect: {
+                destination: "/educator/certificates",
+                permanent: false,
+            },
+        };
+
+
+    } catch (error) {
+        return {
+            notFound: true,
+        };
+    }
+};
